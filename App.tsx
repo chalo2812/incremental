1823b902e968b11d1e8cdc028422d0c954695dd6
Gonzalo Sola <gsola@systechsa.com>
2025-10-22 13:19:20 -0300

    Eliminar ico equivocado. Cambios de tsx

--- App.tsx
+++ App.tsx
@@ -7,21 +7,13 @@ function Page() {
   useEffect(() => {
     async function getTodos() {
       const { data: todos } = await supabase.from('todos').select()
-
       if (todos.length > 1) {
         setTodos(todos)
       }
     }
-
     getTodos()
   }, [])
 
-  return (
-    <div>
-      {todos.map((todo) => (
-        <li key={todo}>{todo}</li>
-      ))}
-    </div>
-  )
+  return ();
 }
 export default Page
